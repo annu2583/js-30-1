d49bf66894422f3e569c12d2fd2755031418f3d0
annu2583 <annuverma8234@gmail.com>
2024-02-15 14:33:48 +0530

    array

---
+++ arrayFunctions.js
@@ -0,0 +1,83 @@
+// let arr=[10,20,30,"a","b","c",true,false]
+// console.log(arr[7]);
+
+/***
+ * 1. ARRAY IS COLLETION OF ITEMS UNDER SINGLE VARIABLE
+ * it's index start from 0 and last index is length of array -1
+ * length is no of items in array
+ * 
+ * 
+ */
+
+// let arr2=[91,20,30,50,61,21,33,25]
+// let arr2=["sakshi","amit","sunil","sunny","balljeet","anu"]
+
+// console.log(arr2.some((item)=>item%2===0))
+
+// console.log(arr2.copyWithin(1,3,4))
+
+// console.log(arr2.entries(7));
+
+// console.log(arr.concat(arr2));
+
+// console.log(arr.length);
+// console.log(arr.at(-2));
+// console.log(arr[-2]); 
+
+
+// for(i=0;i<arr2.length;i++){
+//     console.log(arr2[i]);
+// }
+
+// arr2.map((index,item)=>{
+//     // console.log(index,item);
+//     if(index%2===0){
+//         console.log("even num");
+//     }
+//     else{
+//         console.log("odd num");
+//     }
+// })
+
+// let data=arr2.map((item)=>item%2===0)
+// let data=arr2.filter((item)=>
+// {
+//     if(item%2===0){
+//         return item
+//     }
+
+// }
+// )
+// console.log(data);
+
+
+let arr=[4,67,90,46,56,"ayaan","kajal","floor"];
+let arr2=["closure","formal","dual","coma"]
+for(i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+// console.log(arr.length);
+// console.log(arr[3]);
+// console.log(arr.concat(arr2));
+
+// console.log(arr.entries(5));
+// // console.log(arr.copyWithin(2,5,6));
+// arr.map((item,index)=>{
+//     console.log(item,index);
+//     if (item%2===0) {
+//         console.log("even no.");
+//     }
+//     else{
+//         console.log("odd no.");
+//     }
+// })
+
+// let data=arr.map((item)=>item%2===0)
+// let data=arr.some((item)=>
+// {
+//     if (item%2===0) {
+//         return item
+//     }
+// }
+// )
+// console.log(data);

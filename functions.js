80433faca196170a4bccee6883dcb022dbaa5177
annu2583 <annuverma8234@gmail.com>
2024-02-15 11:53:56 +0530

    functions

--- functions.js
+++ functions.js
@@ -4,7 +4,6 @@
 function handleClick() {
     console.log("here this fuction is invoked");
 }
-handleClick()
 
 // function with parameter
 // function sum(x, y) {
@@ -75,27 +74,111 @@ handleClick()
 //    square(a,b)
 
  function findsquare(numb) {
-   return numb*numb
+  console.log(numb*numb);
+  //  return numb*numb
  }
- console.log(findsquare(6));
- console.log(findsquare(98));
- console.log(findsquare(46));
- console.log(findsquare(23));
+//  console.log(findsquare(6));
+//  console.log(findsquare(98));
+//  console.log(findsquare(46));
+//  console.log(findsquare(23));
 
 
  function recarea(a,b) {
-    return a*b
+  console.log(a*b);
+
+    // return a*b
  }
- console.log("The area of rectangle is",recarea(40,60));
- console.log("The area of rectangle is",recarea(30,60));
- console.log("The area of rectangle is",recarea(40,20));
+//  console.log("The area of rectangle is",recarea(40,60));
+//  console.log("The area of rectangle is",recarea(30,60));
+//  console.log("The area of rectangle is",recarea(40,20));
  
 
+//  let n= prompt("Enter number")
  
-function CheckEven(n){
-  return n%2==0;
+// function CheckEven(n){
+//   // return 
+//    if(n%2==0){
+//     console.log("Even no"); 
+//    }
+//    else{
+//     console.log("odd no"); 
+//    }
+// }
+
+// console.log(CheckEven(89));
+// console.log(CheckEven(90));
+
+
+// function hello(){
+//   console.log("hello");
+
+//   function hi(){
+//     console.log("hi");
+//   }
+
+//   function bye(){
+//     console.log("bye");
+//     hi()
+//   }
+// bye()
+// }
+
+// function hey(){
+//   console.log("hey") ;
+//   hello()
+// }
+
+// hey()
+
+/** anonymous */
+// const name=function(){
+//   console.log("my name is sakshi");
+// }
+
+// name()
+
+/** arrow function */
+
+const checkData=()=>{
+  console.log("hey");
 }
-console.log(CheckEven(45));
-console.log(CheckEven(89));
-console.log(CheckEven(90));
+// checkData()
 
+
+/** self calling function */
+
+(()=>{
+
+  console.log("hi i  am here self called");
+
+})()
+
+
+// types of function and its benefit
+
+
+function hello() {
+  console.log("hello");
+
+  function me() {
+    console.log("me");
+  }
+
+}
+// hello()
+
+// const name=function(){
+//   console.log("Here I am");
+// }
+// name()
+
+// const classroom=()=>{
+//    console.log("us");
+// }
+// classroom()
+
+
+(()=>{
+  console.log("This is a self calling");
+}
+)()

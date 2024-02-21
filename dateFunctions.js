c51cf26d0a04b06aeadabbf380b0be011dfe1a0d
annu2583 <annuverma8234@gmail.com>
2024-02-21 12:42:06 +0530

    maths

--- dateFunctions.js
+++ dateFunctions.js
@@ -1,22 +1,32 @@
 // const date= new Date("2024-04-20")
-const date= new Date()
+// const date= new Date()
+// console.log(date);
+// console.log(date.getDate());
+// console.log(date.getMonth());
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getMilliseconds());
+// console.log(date.getTime());
+
+// date.setDate("25")
+// date.setMonth("5")
+// console.log(date.getMonth());
+
+
+
+const date=new Date()
 console.log(date);
 console.log(date.getDate());
-console.log(date.getMonth());
 console.log(date.getFullYear());
+console.log(date.getMonth());
 console.log(date.getHours());
-console.log(date.getMinutes());
 console.log(date.getMilliseconds());
+console.log(date.getSeconds());
 console.log(date.getTime());
 
-date.setDate("25")
-date.setMonth("5")
-console.log(date.getMonth());
 
-
-
-// const date=new Date()
-// console.log(date());
-
-
-
+date.setHours("15")
+console.log(date.getHours());
+date.setDate("23")
+console.log(date.getDate());

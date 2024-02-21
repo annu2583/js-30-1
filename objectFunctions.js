a331edee3259c01171477d362321a2409d7a19fe
annu2583 <annuverma8234@gmail.com>
2024-02-21 14:57:17 +0530

    objects

--- objectFunctions.js
+++ objectFunctions.js
@@ -1,36 +1,3 @@
-// let num =Math.ceil(5.789)
-// console.log(num);
-
-
-// let num =Math.floor(5.789)
-// console.log(num);
-
-// let num =Math.max(1,45,65,-45);
-// console.log(num);
-
-// let num =Math.min(1,45,65,-45);
-// console.log(num);
-
-// let num =Math.pow(4,5);
-// console.log(num);
-
-// let num =Math.random();
-// console.log(num*34);
-
-
-// let num =Math.round(45.788);
-// console.log(num);
-
-
-// let num =Math.sign(-56);
-// console.log(num);
-
-// let num =Math.sqrt(369);
-// console.log(num);
-
-let num =Math.trunc(344.566);
-console.log(num);
-
 
 let obj={
     name:"Sakshi",

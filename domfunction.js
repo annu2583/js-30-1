1ca9d7cd0b54d30984f1a181e887fad4c9b0064c
annu2583 <annuverma8234@gmail.com>
2024-03-04 10:07:24 +0530

    index

--- domfunction.js
+++ domfunction.js
@@ -16,3 +16,22 @@ link[0].setAttribute('href','www.google.com')
 
 link[0].style.color="blue"
 btn.style.backgroundColor="red"
+
+
+function handleChange(){
+    let emailInput=document.getElementById('email')
+
+    console.log(emailInput.value,"==here value");
+}
+
+
+// document.querySelector('.join').addEventListener('click',displayData)
+document.querySelector('.join').addEventListener('mouseover',displayData)
+document.querySelector('.join').addEventListener('mouseout',displayData2)
+
+function displayData(){
+    alert("I am hovered")
+}
+function displayData2(){
+    alert("I am out")
+}

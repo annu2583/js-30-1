a0256221d7787ec22a65e3c3ab2e1cbc86eeb028
annu2583 <annuverma8234@gmail.com>
2024-03-20 15:25:17 +0530

    index

--- domfunction.js
+++ domfunction.js
@@ -1,4 +1,4 @@
-document.getElementById("name").innerHTML = "Hello World!";
+// document.getElementById("name").innerHTML = "Hello World!";
 
 
 // let links=document.getElementsByClassName("nav_link")
@@ -7,31 +7,91 @@ document.getElementById("name").innerHTML = "Hello World!";
 // console.log(links);
 
 
-let link=document.querySelectorAll(".nav_link")
-let btn=document.querySelector(".btn_container")
-console.log(link);
+// let link=document.querySelectorAll(".nav_link")
+// let btn=document.querySelector(".btn_container")
+// console.log(link);
 
-console.log(link[0].getAttribute('href')); 
-link[0].setAttribute('href','www.google.com')
+// console.log(link[0].getAttribute('href')); 
+// link[0].setAttribute('href','www.google.com')
 
-link[0].style.color="blue"
-btn.style.backgroundColor="red"
+// link[0].style.color="blue"
+// btn.style.backgroundColor="red"
 
 
-function handleChange(){
-    let emailInput=document.getElementById('email')
+// function handleChange(){
+//     let emailInput=document.getElementById('email')
 
-    console.log(emailInput.value,"==here value");
+//     console.log(emailInput.value,"==here value");
+// }
+
+
+// // document.querySelector('.join').addEventListener('click',displayData)
+// document.querySelector('.join').addEventListener('mouseover',displayData)
+// document.querySelector('.join').addEventListener('mouseout',displayData2)
+
+// function displayData(){
+//     alert("I am hovered")
+// }
+// function displayData2(){
+//     alert("I am out")
+// }
+
+
+
+
+
+
+
+
+let a = "togle_icon"
+console.log(a);
+
+let NavBar = document.querySelector("nav_bar");
+let Navlist = document.querySelector("nav_menu");
+function menu() {
+    
 }
 
+// let hiddenList= document.querySelector('.nav_menu');
+// let toggle= document.querySelector('togle_icon');
 
-// document.querySelector('.join').addEventListener('click',displayData)
-document.querySelector('.join').addEventListener('mouseover',displayData)
-document.querySelector('.join').addEventListener('mouseout',displayData2)
 
-function displayData(){
-    alert("I am hovered")
-}
-function displayData2(){
-    alert("I am out")
-}
+
+
+// toggle.addEventListener('click', function(){
+//     hiddenList.classList.toggle('shown')
+// })
+
+
+let hiddenList = document.querySelector(".nav_menu")
+let toggle = document.querySelector(".togle_icon")
+toggle.addEventListener('click',function(){
+    console.log(hiddenList);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ toggle.js
@@ -0,0 +1,10 @@
+let hiddenList = document.querySelector(".nav_menu")
+let toggle = document.querySelector(".togle_icon")
+toggle.addEventListener('click',function(){
+    // console.log(hiddenList);
+    hiddenList.classList.toggle('shown')
+})
+
+
+let arr=[1,3,45454]
+console.log(arr);

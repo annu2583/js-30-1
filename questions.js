4bf1bd9a752a0780302811857ebc6f4935e56392
annu2583 <annuverma8234@gmail.com>
2024-04-05 11:14:51 +0530

    ques

--- questions.js
+++ questions.js
@@ -126,33 +126,36 @@ let days = ['Sunday', 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday'
 
 console.log("Today is :" + days[day]);
 
-let hours = today.getHours();
-let minutes = today.getMinutes();
-let seconds = today.getSeconds();
-let session = '';
+// let hours = today.getHours();
+// let minutes = today.getMinutes();
+// let seconds = today.getSeconds();
+// let session = '';
 
-if (hours<= 12) {
-  session = "PM";
-}
-else{
-  session = "AM";
-}
+// if (hours>= 12) {
+//   session = "PM";
+// }
+// else{
+//   session = "AM";
+// }
 
 
-if (hours<10) {
-  hours = '0' + hours;
-}
-if (minutes<10) {
-  minutes = '0' + minutes;
-}
+// if (hours<10) {
+//   hours = '0' + hours;
+// }
+// if (minutes<10) {
+//   minutes = '0' + minutes;
+// }
 
-if (seconds<10) {
-  seconds = '0' + seconds;
-}
+// if (seconds<10) {
+//   seconds = '0' + seconds;
+// }
 // let time = today.getHours() + ':' +  today.getMinutes() + ':'+ today.getSeconds() + ' ' + session;
-let time = hours + ':' + minutes + ':'+ seconds + ' ' + session;
-// let time = today.toLocaleTimeString();
+// let time = hours + ':' + minutes + ':'+ seconds + ' ' + session;
+let time = today.toLocaleTimeString();
 console.log(time);
+
+
+
 
 
 let current = new Date();
@@ -180,3 +183,5 @@ console.log(mm);
 
 mm = date + '-' + month + '-' + year ;
 console.log(mm);
+
+
